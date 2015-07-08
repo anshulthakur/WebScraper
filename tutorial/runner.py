fe8ef67163dedb6774ff925b5b8363a09b3d8ff0
Anshul Thakur <athakur@cdot.in>
2015-07-08 10:16:04 +0530

    Runner script accepts a space separated list of URLs to scrape.

--- tutorial/runner.py
+++ tutorial/runner.py
@@ -9,18 +9,34 @@ import os
 path = os.path.dirname(os.path.abspath('__file__'))
 sys.path.append(path)
 
-import scrapy
 from scrapy.crawler import CrawlerProcess
 
 from scrapy.utils.project import get_project_settings
 
-from tutorial.spiders.general import GeneralSpider
 
-#process = CrawlerProcess({
-#        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1'
-#    })
+#===================================
+# Class Declarations
+#===================================
+class InputError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return str(self.msg)
 
+#===================================
+# Main script
+#===================================
+urls = []
+   
+if len(sys.argv) < 2:
+    raise InputError('At least one URL must be provided for crawler')
+
+for i in range(1, len(sys.argv)):
+    print sys.argv[i]
+    urls.append(sys.argv[i])
+
+#Crawl the given set of URLs
+#NOT validating the URLs here. The URLParse method would implicitly do that.
 process = CrawlerProcess(get_project_settings())
-
-process.crawl('General Spider', urls=['http://piratelearner.com'])
+process.crawl('General Spider', urls=urls)
 process.start()
