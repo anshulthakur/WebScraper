fa53dbec733431b1eb56d630fd7dc2144dc7b43a
Anshul Thakur <athakur@cdot.in>
2017-10-09 14:47:56 +0530

    Parse domain, skip subdomain and a few known redundant patterns.

--- stocks/stocks/spiders/general.py
+++ stocks/stocks/spiders/general.py
@@ -7,6 +7,9 @@ from stocks.items import GeneralItem
 from scrapy.utils.project import get_project_settings
 from scrapy.spidermiddlewares.offsite import OffsiteMiddleware
 
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
+
 import re
 from urlparse import urlparse
 
@@ -45,8 +48,6 @@ class GeneralSpider(scrapy.Spider):
             middleware.spider_opened(self)
 
   def parse(self, response):
-    #Save the URL of response
-    print response.url
     #Right now, all I want to do is make a list of URLs that can be accessed from mere crawling
 
     # for selection in response.xpath('//a'):
@@ -56,17 +57,35 @@ class GeneralSpider(scrapy.Spider):
     #     item['url'] = selection.xpath('./@href').extract()
     #     yield item
     #A generator kind of remembers the entire thing, so when we finally break out, we get to next line of code.
+    filters = ['^.*\//blog\..*',
+               '^.*/accounts/login/.*',
+               '^.*/accounts/twitter/.*',
+               '^.*/accounts/facebook/.*',
+               '^.*/accounts/google/.*', #Nuisance
+               '^.*\.php\?.*', #Python based sites don't call into PHP code (except if its a FB script)
+               '.*#.*', #Ignore Anchor Links to same page
+              ]
+
+    compound = "(" + ")|(".join(filters)+")"
 
     #Now, we also want to feed our crawler new URLs
     for selection in response.xpath('//a'):
       if selection.xpath('./@href').extract():
-        item = response.urljoin(selection.xpath('./@href').extract()[0])
-        yield SplashRequest(item, self.parse,
-                          args = {
-                            'wait': 0.5,
-                            'timeout' : 120,
-                          },
-                          endpoint = 'render.html',
-                          slot_policy=scrapy_splash.SlotPolicy.PER_DOMAIN,
-                          )
+        if re.match(compound, selection.xpath('./@href').extract()[0]) is None:
+          item = response.urljoin(selection.xpath('./@href').extract()[0])
+          yield SplashRequest(item, self.parse,
+                            args = {
+                              'wait': 0.5,
+                              'timeout' : 120,
+                            },
+                            endpoint = 'render.html',
+                            slot_policy=scrapy_splash.SlotPolicy.PER_DOMAIN,
+                            )
+
+    #Save the URL of response
+    item = GeneralItem()
+    item['title'] = response.css('title::text').extract()[0]
+    item['url'] = response.url
+    item['desc'] = response.status
+    yield item
 
